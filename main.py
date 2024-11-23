e83cd85727f23ad1b9a835200658538c302a3f22
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-22 22:04:16 -0500

    Doc: added documentation for the triangle area

--- main.py
+++ main.py
@@ -11,4 +11,13 @@ def rectangle_area(length, width):
     return length * width
 
 def triangle_area(base, height):
+    """Calculate the area of a triangle
+
+    Args:
+        base (float): the base of the triangle
+        height (float): the height of the triangle
+
+    Returns:
+        float: the area of a triangle
+    """
     return 0.5 * (base * height)
