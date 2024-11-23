ca964fb4adfd6df2ddc5e399b96b9e7b0b819eb5
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-22 21:58:16 -0500

    Test: added test for calculating the area of a triangle

--- test.py
+++ test.py
@@ -3,3 +3,8 @@ def test_calculate_rectangle_area():
     assert rectangle_area(2,10) == 20
     assert rectangle_area(5,10) == 50
     assert rectangle_area(10,0) == 0
+
+def  test_calculate_triangle_area():
+    assert triangle_area(10 ,5) == 25.0
+    assert triangle_area(0, 5) == 0
+    assert triangle_area(3, 6) == 9.0
