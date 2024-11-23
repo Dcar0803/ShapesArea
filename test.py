337ca7796f037226c05c68c1ffe8073c46c7d891
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-22 22:09:51 -0500

    Test: Added test for calculating the area of a circle

--- test.py
+++ test.py
@@ -9,3 +9,8 @@ def  test_calculate_triangle_area():
     assert triangle_area(10 ,5) == 25.0
     assert triangle_area(0, 5) == 0
     assert triangle_area(3, 6) == 9.0
+
+def test_calculate_circle_area():
+    assert circle_area(7) == pytest.approx(153.938, rel=1e-3)
+    assert circle_area(0) == 0
+    assert circle_area(1) == pytest.approx(3.14159, rel=1e-5)
