e2a9d333dd5e48f3e922ac11f6dbb53bac48cf4f
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-11-22 21:41:58 -0500

    Doc: added documentation for the area of a rectangle

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 def rectangle_area(length, width):
+    """Calculate the area of a rectangle
+
+    Args:
+        length (float): the length of the rectangle
+        width (float): the width of the rectangle
+
+    Returns:
+        float: The area of a rectangle
+    """
     return length * width
